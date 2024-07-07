268daebf394e4977e0ab9fc07fcbda4f0cf944ca
Meron <meronmuleta25@gmail.com>
2024-07-07 11:28:48 -0700

    added shopping list manager

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,34 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            add = input("Enter the item name to add: ")
+            shopping_list.append(add)
+            pass
+        elif choice == '2':
+            remove = input("enter the item name to remove: ")
+            shopping_list.remove(remove)
+            pass
+        elif choice == '3':
+            for item in shopping_list:
+                print(item)
+            pass
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
+
