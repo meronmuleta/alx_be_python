48bcf5316a56cc30f08633867d0e2934f5fbdec2
Meron <meronmuleta25@gmail.com>
2024-07-06 17:34:25 -0700

    modified

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,5 +9,5 @@ def perform_operation(num1, num2, operation):
         case 'divide':
             if num2 == 0:
                 return "Division by zero error"
-            else:
+            elif num2 != 0:
                 return num1 / num2
