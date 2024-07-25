b79ad776ae579cb53f4b11db105ab4f6251446c2
Meron <meronmuleta25@gmail.com>
2024-07-25 13:56:39 -0700

    add polymorphism and class static together

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,18 @@
+import math
+
+class Shape:
+    def area(self):
+        raise NotImplementedError
+class Rectangle(Shape):
+    def __init__(self,length,width):
+        self.length = length
+        self.width = width
+    def area(self):
+        area = self.length * self.width
+        return area
+class Circle(Shape):
+    def __init__(self,radius):
+        self.radius = radius
+    def area(self):
+        area = math.pi * (self.radius)**2
+        return area
